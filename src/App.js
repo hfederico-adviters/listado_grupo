a04757f21552f1bbe07462749303bff1e847d42d
Capacitaciones 5 <capacitaciones5@adviters.com>
2023-03-29 14:14:43 -0300

    se termino el layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./pages/login/login";
 
 function App() {
   return (
-    <Home/>
+    <Login/>
   );
 }
 
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,56 @@
-import Layout from "../../components/Layout/layout"
+import { Field, Form } from "react-final-form";
+import Layout from "../../components/Layout/layout";
 
-const Login= ()=>{
-    return    (   
+const Login = () => {
+  return (
     <Layout titulo="Login">
-    <div>
-      Hola este es el login
-    </div>
-  </Layout>
-  )
-}
+      <Form
+        onSubmit={(values) => console.log(values)}
+        render={({ handleSubmit }) => (
+          <div
+            style={{ display: "flex", flexDirection: "column", width: "100%" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                margin: "8px",
+              }}
+            >
+              <Field
+                name="email"
+                component="input"
+                placeholder="Correo"
+                style={{ width: "300px", height: "30px" }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                margin: "8px",
+              }}
+            >
+              <Field
+                name="contrasenia"
+                component="input"
+                placeholder="contraseña"
+                style={{ width: "300px", height: "30px" }}
+                
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <button onClick={handleSubmit}>enviar</button>
+            </div>
+          </div>
+        )}
+      />
+    </Layout>
+  );
+};
 export default Login;
